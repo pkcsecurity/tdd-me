83cd275bdab36916179bcc081abf73e8cf58ce5c
Ryan Asensio <ryan.asensio@gmail.com>
2018-04-13 12:13:44 -0700

    Tests posting with parameters.

--- src/tdd_clj/core.clj
+++ src/tdd_clj/core.clj
@@ -1,7 +1,22 @@
 (ns tdd-clj.core
   (:require [compojure.core :refer :all]
+            [cheshire.core :as cheshire]
             [compojure.route :as route]))
 
+(defn parse-body [body]
+  (cheshire/parse-string (slurp body) true))
+
+(defn handleposthere [body]
+  (if body
+    {:body (merge {:received? true} (parse-body body))}
+    "No params. That's OK"))
+
+(defn handlehome [query-string]
+  (if query-string
+    (str "Hello query string:" query-string)
+    "Hello World"))
+
 (defroutes app
-  (GET "/" [] "Hello World")
-  (route/not-found "<h1>Page not found</h1>"))
+  (GET "/" {:keys [query-string] :as request} (handlehome query-string))
+  (POST "/posthere" {:keys [body] :as request} (handleposthere body))
+  (route/not-found "Page Not Found"))
--- test/tdd_clj/core_test.clj
+++ test/tdd_clj/core_test.clj
@@ -1,11 +1,34 @@
 (ns tdd-clj.core-test
   (:require [clojure.test :refer :all]
             [tdd-clj.core :as core]
+            [cheshire.core :as cheshire]
             [ring.mock.request :as mock]))
 
 (deftest simple-get-test
-  (testing "simple get"
+  (testing "simple get with no params"
     (let [response (core/app (mock/request :get "/"))]
       (is (= "Hello World" (response :body))
-          (= 200 (response :status))
-          ))))
+          (= 200 (response :status)))))
+  (testing "simple get with query string"
+    (let [response (core/app (mock/request :get "/" {:hi "ok"}))]
+      (is (= "Hello query string:hi=ok" (response :body))
+          (= 200 (response :status))))))
+
+(deftest simple-post-test
+  (testing "simple post with params"
+    (let [params {:param1 "one one" :another 7}
+          response (core/app
+                    (-> (mock/request :post "/posthere" )
+                        (mock/content-type "application/json")
+                        (mock/body (cheshire/generate-string params))))]
+      (is (= true (get-in response [:body :received?])))
+      (= 200 (response :status)))))
+
+(deftest not-found-test
+  (testing "not found"
+    (let [response (core/app (mock/request :get "/lol"))]
+      (is (= "Page Not Found" (response :body))
+          (= 200 (response :status))))
+    (let [response (core/app (mock/request :post "/lol"))]
+      (is (= "Page Not Found" (response :body))
+          (= 200 (response :status))))))
